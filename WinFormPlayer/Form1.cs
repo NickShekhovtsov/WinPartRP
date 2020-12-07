824d977e2c3977ad0b2cab4f0fed97797794fb8a
Nikolay Shekhovsov <killerkol@mail.ru>
2020-12-08 02:25:46 +0300

    добавил сокеты
    не работают корректно

--- WinFormPlayer/Form1.cs
+++ WinFormPlayer/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,22 +13,24 @@ namespace WinFormPlayer
 {
     public partial class Form1 : Form
     {
+        private SocketManager client = new SocketManager();
         private AudioPlayer Player;
-        
+
         public Form1()
         {
 
             InitializeComponent();
             Player = new AudioPlayer();
-            
+            InputServerWorkAsync();
+
         }
-       
+
 
         private void button1_Click(object sender, EventArgs e)
         {
-            using (OpenFileDialog dialog = new OpenFileDialog() { Multiselect = true, Filter="Audio Files|*.mp3" })
+            using (OpenFileDialog dialog = new OpenFileDialog() { Multiselect = true, Filter = "Audio Files|*.mp3" })
             {
-                if (dialog.ShowDialog()==DialogResult.OK)
+                if (dialog.ShowDialog() == DialogResult.OK)
                 {
                     Player.LoadAudio(dialog.FileNames);
                     listBox1.Items.Clear();
@@ -40,7 +43,7 @@ namespace WinFormPlayer
 
         private void button3_Click(object sender, EventArgs e)
         {
-            
+
             if (button3.Text == "Play")
             {
                 Player.Play();
@@ -55,6 +58,7 @@ namespace WinFormPlayer
                 return;
             }
             
+
         }
 
         private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
@@ -62,18 +66,18 @@ namespace WinFormPlayer
             if (((ListBox)sender).SelectedItem == null)
                 return;
             Player.SelectAudio(((ListBox)sender).SelectedIndex);
-            
+
             button3.Text = "Stop";
-            
+
 
         }
-        
+
 
         private void buNext_Click(object sender, EventArgs e)
         {
             Player.Next();
-            if (listBox1.SelectedIndex<listBox1.Items.Count-1)
-            listBox1.SetSelected(++listBox1.SelectedIndex, true);
+            if (listBox1.SelectedIndex < listBox1.Items.Count - 1)
+                listBox1.SetSelected(++listBox1.SelectedIndex, true);
             else
                 listBox1.SetSelected(0, true);
             button3.Text = "Stop";
@@ -89,9 +93,65 @@ namespace WinFormPlayer
             button3.Text = "Stop";
         }
 
-       
+
 
         private void trackBar1_Scroll(object sender, EventArgs e) => Player.Volume = ((TrackBar)sender).Value;
-        
+
+
+
+        private void InputServerWork()
+        {
+
+            while (true)
+                if (client.client.Available > 0)
+                {
+                    byte[] data;
+                    // получаем ответ
+                    data = new byte[256]; // буфер для ответа
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; // количество полученных байт
+
+                    do
+                    {
+                        bytes = client.client.Receive(data, data.Length, 0);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (client.client.Available > 0);
+
+                    client.msg = builder.ToString();
+
+                    if (client.msg == "play")
+                    {
+                        button3_Click(button3, EventArgs.Empty);
+
+                        
+                    }
+
+                    if (client.msg == "stop")
+                    {
+                        button3_Click(button3, EventArgs.Empty);
+
+                        
+                    }
+
+                    if (client.msg == "next")
+                    {
+                        buNext_Click(buNext, EventArgs.Empty);
+                    }
+
+                    if (client.msg == "prev")
+                    {
+                        buPrev_Click(buPrev, EventArgs.Empty);
+                    }
+
+                }
+                else Thread.Sleep(100);
+        }
+
+        private async void InputServerWorkAsync()
+        {
+            await Task.Run(() => InputServerWork());
+        }
+
     }
 }
---
+++ WinFormPlayer/SocketManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace WinFormPlayer
+{
+    class SocketManager
+    {
+        public Socket client;
+        public string msg;
+        public SocketManager()
+        {
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            client.Connect("127.0.0.1", 8000);
+        }
+        
+    }
+}
